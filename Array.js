cc2db13ba82d276bde84292301eef5694f2e1b0a
Zikos2906 <bihan.banerjee2906@gmail.com>
2025-03-24 17:57:00 +0530

    Add files via upload

--- Array.js
+++ Array.js
@@ -1,1 +1,28 @@
-console.log("I am Bihan")
+// // console.log("Lets Learn Array") 
+// // console.log("Bihan Banerjee")
+
+// // var friend = "Arihaan"
+// // console.log(friend)
+
+// // var number = 134
+// // console.log(number)
+
+// var friends = ["Arihaan","Lalit","Ashwin","Vihaan","Teja"]
+// console.log(friends[0])
+// console.log(friends[1])
+// console.log(friends[2])
+// console.log(friends[3])
+// console.log(friends[4])
+
+
+// friends.push("Kiaan")
+// console.log(friends)
+
+// friends.pop()
+// console.log(friends)
+
+ var number = [1 , 2 , 3 , 4 , 5]
+ console.log(number)
+ console.log(number[0])
+ number.reverse()
+ console.log(number)
