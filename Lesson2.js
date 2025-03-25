d2a2b2eb8d13cfa5fd5d000be844381668906fc6
Zikos2906 <bihan.banerjee2906@gmail.com>
2025-03-25 19:10:34 +0530

    Add files via upload

---
+++ Lesson2.js
@@ -0,0 +1,28 @@
+
+// var fruit = ["Apple","Banana","Mango","Orange"]
+// console.log(fruit)
+//  console.log(fruit[0])
+// fruit.pop()
+// console.log(fruit)
+//fruit.reverse()
+// console.log(fruit)
+
+// var a = 5
+// while(a<10){
+//     console.log("Hello")
+//     a++
+// }
+
+for(var a=1; a<5 ; a++){
+    console.log("Inside for loop when a value is: "+a);
+
+}
+
+console.log("Outside for loop")
+
+var Worldcup = ["Brazil","Argintina","Portugal","Japan"]
+for(var a = 0;a<Worldcup.length;a++){
+    console.log(Worldcup[a])
+}
+
+
