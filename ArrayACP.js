b1c3dc8da5add463731f9d742b2b31247bcc976e
Zikos2906 <bihan.banerjee2906@gmail.com>
2025-04-02 18:45:14 +0530

    Add files via upload

---
+++ ArrayACP.js
@@ -0,0 +1,14 @@
+var friends = ["Lalit","Suhas","Vihaan","Ashwin"]
+console.log(friends[0])
+console.log(friends[1])
+console.log(friends[2])
+console.log(friends[3])
+
+friends.pop()
+console.log(friends)
+
+friends.push("Suhas")
+console.log(friends)
+
+friends.reverse()
+console.log(friends)
