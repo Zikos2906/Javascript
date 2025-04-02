11fc407736c17beb978b151709c507276a512b6d
Zikos2906 <bihan.banerjee2906@gmail.com>
2025-04-02 19:07:59 +0530

    Add files via upload

--- Array.js
+++ Array.js
@@ -1,28 +1,13 @@
-// // console.log("Lets Learn Array") 
-// // console.log("Bihan Banerjee")
-
-// // var friend = "Arihaan"
-// // console.log(friend)
-
-// // var number = 134
-// // console.log(number)
-
-// var friends = ["Arihaan","Lalit","Ashwin","Vihaan","Teja"]
-// console.log(friends[0])
-// console.log(friends[1])
-// console.log(friends[2])
-// console.log(friends[3])
-// console.log(friends[4])
+var fruit = ["Apple", "Banana","Mango"]
+var number = [1,2,3,4,5,6,7,8,9,10]
+var name = ["Ashwin", "Teja", "Arihaan","Dakshesh","Vihaan" ]
 
 
-// friends.push("Kiaan")
-// console.log(friends)
 
-// friends.pop()
-// console.log(friends)
 
- var number = [1 , 2 , 3 , 4 , 5]
- console.log(number)
- console.log(number[0])
- number.reverse()
- console.log(number)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+
