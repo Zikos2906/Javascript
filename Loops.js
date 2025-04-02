34129b10bf6fa1b1cbe1d9db5378819b873690ed
Zikos2906 <bihan.banerjee2906@gmail.com>
2025-04-02 18:57:31 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,12 @@
+
+for(var a=1; a<5 ; a++){
+    console.log("Inside for loop when a value is: "+a);
+
+}
+
+console.log("Outside for loop")
+
+var Worldcup = ["Brazil","Argintina","Portugal","Japan"]
+for(var a = 0;a<Worldcup.length;a++){
+    console.log(Worldcup[a])
+}
